0212da12e3dd155a814949ff29bedd9aa8886c31
Pedro9185 <pedrocatarina.ip@gmail.com>
2025-07-01 19:57:38 -0300

    First Commit

---
+++ src/Login.tsx
@@ -0,0 +1,11 @@
+function Login() {
+  return (
+    <>
+      <div>
+        <h1>Aqui vai ser a Página de Login</h1>
+      </div>
+    </>
+  );
+}
+
+export default Login;
---
+++ src/dados/usuarios.js
@@ -0,0 +1,12 @@
+export const usuarios = [
+  {
+    id: 1,
+    email: "user@teste.com",
+    senha: "user123",
+  },
+  {
+    id: 2,
+    email: "user2@teste.com",
+    senha: "user123",
+  },
+];
