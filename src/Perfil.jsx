8164d6609648fb184c8fc6ab576b16819637d3ff
guztamq <gustavoomarques2484@gmail.com>
2025-07-08 14:32:15 -0300

    Criação do componente Perfil

---
+++ src/Perfil.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Perfil.css';
+
+const Perfil = () => {
+    const usuario = {
+        nome: {nome},
+        membroDesde: {membroDesde},
+        avaliacao: {avaliacao},
+        quantidadeAvaliacoes: {quantidadeAvaliacoes},
+        trocas: {trocas},
+        local: {local},
+        foto: {foto},
+    };
+    const itens = [
+        {}
+    ]
+}
