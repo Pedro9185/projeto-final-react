2accf1106099aec8a107b5e1f23b9d2ea7d45489
guztamq <gustavoomarques2484@gmail.com>
2025-07-09 15:16:46 -0300

    Criando componente tabs para a pagina de usuário

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,9 @@
+export default function Tabs() {
+    return (
+        <div className="Tabs">
+            <button>Meus Itens</button>
+            <button>Propostas</button>
+            <button>Histórico</button>
+        </div>
+    );
+}
