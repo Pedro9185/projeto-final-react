2b8914bbf087af2af7fd7bcf9d7033641614418e
guztamq <gustavoomarques2484@gmail.com>
2025-07-09 15:13:03 -0300

    Refazendo componente UserCard

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,18 @@
+export default function UserCard () {
+    return (
+        <div className="user-card">
+        <img src="" alt="Avatar" />
+        <div>
+            <h2>User</h2>
+            <p>Membro desde </p>
+            <p>avaliações</p>
+            <p>trocas realizadas</p>
+            <p>Cidade</p>
+            </div>
+            <div>
+                <button>Configurações</button>
+                <button>Novo Item</button>
+            </div>
+            </div>
+        );
+}
