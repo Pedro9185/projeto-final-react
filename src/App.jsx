55be67e354171d98b5137809a6f81ae52d00cb0d
Pedro9185 <pedrocatarina.ip@gmail.com>
2025-07-15 07:39:41 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Login from "./Pages/Login/Login";
 import Home from "./Pages/Home/Home";
 import UserCard from './components/UserCard'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
