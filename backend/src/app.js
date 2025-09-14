e4bb0c0f00c0aff46dc801ca8fb0ce1365b27078
ysabeleeee <ysabelle.da.hora2007@gmail.com>
2025-09-14 17:30:38 -0300

    dependências da integração

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const app = require('cors')();
 const UserRoutes = require('./router/UserRoutes');
 const TradeRoutes = require('./router/TradeRoutes');
 
 =======
+app.use(cors());
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('API is running');
